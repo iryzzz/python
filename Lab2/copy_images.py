4f1570a343f19b640b49bd651d7858c226b40bf4
iryzzz <58042184+iryzzz@users.noreply.github.com>
2024-05-10 23:21:05 +0400

    fixed

--- Lab2/copy_images.py
+++ Lab2/copy_images.py
@@ -1,5 +1,6 @@
-import os
 import csv
+import logging
+import os
 import shutil
 
 from annotation import get_paths, get_abs_paths
@@ -13,14 +14,20 @@ def create_dataset_copy(old_path: str, new_path: str, classes: list[str]):
     :param classes: Классы изображений
     :return:
     """
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
-    for class_name in classes:
-        class_path = os.path.join(old_path, class_name)
-        images = os.listdir(os.path.join(old_path, class_name))
-        for image in images:
-            shutil.copyfile(os.path.join(class_path, image),
-                            os.path.join(new_path, f"{class_name}_{image}"))
+    try:
+        logging.info("Start copying images")
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+        for class_name in classes:
+            logging.info(f"Handled class: {class_name}")
+            class_path = os.path.join(old_path, class_name)
+            images = os.listdir(os.path.join(old_path, class_name))
+            for image in images:
+                shutil.copyfile(os.path.join(class_path, image),
+                                os.path.join(new_path, f"{class_name}_{image}"))
+        logging.info("Copying of images is completed")
+    except Exception as exc:
+        logging.error(f"Error: {exc}")
 
 
 def create_copy_annotation(dataset_dir: str, classes: list[str], save_path: str):
@@ -31,18 +38,19 @@ def create_copy_annotation(dataset_dir: str, classes: list[str], save_path: str)
     :param save_path: Путь к аннотации
     :return:
     """
-    if os.path.exists(save_path):
-        os.remove(save_path)
-    paths = get_paths(dataset_dir)
-    abs_paths = get_abs_paths(dataset_dir)
-    for class_name in classes:
-        with open(save_path, 'a') as csv_file:
-            writer = csv.writer(csv_file, delimiter='\t', lineterminator='\n')
-            for path, abs_path in zip(paths, abs_paths):
-                if class_name in path:
-                    writer.writerow([abs_path, path, class_name])
-
-
-if __name__ == "__main__":
-    create_dataset_copy("dataset", "dataset_copy", ["cat", "dog"])
-    create_copy_annotation("dataset_copy", ["cat", "dog"], "annotation_copy.csv")
+    try:
+        logging.info("Start of annotation creation")
+        if os.path.exists(save_path):
+            os.remove(save_path)
+        paths = get_paths(dataset_dir)
+        abs_paths = get_abs_paths(dataset_dir)
+        for class_name in classes:
+            logging.info(f"Handled class: {class_name}")
+            with open(save_path, 'a') as csv_file:
+                writer = csv.writer(csv_file, delimiter='\t', lineterminator='\n')
+                for path, abs_path in zip(paths, abs_paths):
+                    if class_name in path:
+                        writer.writerow([abs_path, path, class_name])
+        logging.info("Annotation creation is completed")
+    except Exception as exc:
+        logging.error(f"Error: {exc}")
---
+++ Lab2/main_lab2.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+import logging
+
+import annotation
+import copy_images
+import random_dataset
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='lab2.log', level=logging.INFO)
+    parser = ArgumentParser()
+    parser.add_argument('func', type=str, help='1 - create annotation for original dataset;\n'
+                                               '2 - create copy of dataset and annotation;\n'
+                                               '3 - create random dataset and annotation.',
+                        required=True, choices=['1', '2', '3'])
+    parser.add_argument('-d', '--data_dir', type=str, help='Directory of dataset', required=True)
+    parser.add_argument('-c', '--classes', nargs='+', help='List of classes', required=True)
+    parser.add_argument('-a', '--annotation', type=str, help='Path to annotation', required=True)
+    args = parser.parse_args()
+
+    if args.func == '1':
+        annotation.create_annotation(args.data_dir, args.classes, args.annotation)
+    elif args.func == '2':
+        copy_images.create_dataset_copy(args.data_dir, f"{args.data_dir}_copy", args.classes)
+        copy_images.create_copy_annotation(f"{args.data_dir}_copy", args.classes, args.annotation)
+    elif args.func == '3':
+        cls_dict = random_dataset.create_dataset_random(args.data_dir, f"{args.data_dir}_random", args.classes)
+        random_dataset.create_random_annotation(f"{args.data_dir}_random", cls_dict, args.annotation)

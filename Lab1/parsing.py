d6f0c792d3c7f4b8d9a970435cc741153d434a0a
iryzzz <58042184+iryzzz@users.noreply.github.com>
2024-05-10 23:17:42 +0400

    fixed

--- Lab1/parsing.py
+++ Lab1/parsing.py
@@ -1,46 +1,76 @@
+from argparse import ArgumentParser
+import logging
 import os
 
 from bs4 import BeautifulSoup
+from fake_headers import Headers
 import requests
 
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+
+def create_dir(path: str):
+    """
+    Создание директории
+    :param path: Путь к директории
+    :return:
+    """
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+            logging.info(f"Creating dir: {path}")
+    except Exception as exc:
+        logging.error(f"Error: {exc}")
 
 
-def download_images(path: str, key: str, page: int, count: int = 1000) -> int:
+def download_images(path: str, key: str, page: int, count: int = 1000,
+                    url: str = "https://yandex.ru/images/"):
     """
     Парсинг изображений
     :param path: Путь к папке с изображениями
     :param key: Класс изображения
     :param page: Страница старта
     :param count: Количество скачиваемых изображений
-    :return: Страница окончания
+    :param url: url
+    :return:
     """
-    index = len(os.listdir(path))
-    if index >= count:
-        return page
-    while True:
-        url = f'https://yandex.ru/images/search?p={page}&text={key}'
-        response = requests.get(url, headers=HEADERS)
-        soup = BeautifulSoup(response.text, 'lxml')
-        images = soup.find_all('img', 'ContentImage-Image_clickable')
-        for image in images:
-            if index >= count:
-                return page
-            image_url = image.get("src")
-            if image_url and not image_url.startswith("data:"):
-                picture = requests.get(f"https:{image_url}", HEADERS)
+    try:
+        logging.info("Start downloading images")
+        index = len(os.listdir(path))
+        if index >= count:
+            logging.info("Image download completed")
+            return
+        while True:
+            header = Headers(headers=False).generate()
+            url = url + f'search?p={page}&text={key}'
+            response = requests.get(url, headers=header)
+            soup = BeautifulSoup(response.text, 'lxml')
+            images = soup.find_all('img', 'ContentImage-Image_clickable')
+            for image in images:
+                if index >= count:
+                    logging.info("Image download completed")
+                    return
+                image_url = image.get("src")
+                if image_url and not image_url.startswith("data:"):
+                    picture = requests.get(f"https:{image_url}", header)
 
-                with open(os.path.join(f"{path}/{str(index).zfill(4)}.jpg"), "wb") as f:
-                    f.write(picture.content)
+                    with open(os.path.join(path, f"{str(index).zfill(4)}.jpg"), "wb") as f:
+                        f.write(picture.content)
 
-                index += 1
-        page += 1
+                    index += 1
+            logging.info(f"Page: {page}, saved images: {index}")
+            page += 1
+    except Exception as exc:
+        logging.error(f"Error: {exc}")
 
 
 if __name__ == "__main__":
-    if not os.path.exists("dataset/cat"):
-        os.makedirs("dataset/cat")
-    if not os.path.exists("dataset/dog"):
-        os.makedirs("dataset/dog")
-    print(download_images("dataset/cat", 'cat', 1, 1000))
-    print(download_images("dataset/dog", 'dog', 1, 1000))
+    logging.basicConfig(filename='lab1.log', level=logging.INFO)
+    parser = ArgumentParser()
+    parser.add_argument('data_dir', type=str, help='Directory for dataset')
+    parser.add_argument('-c', '--classes', nargs='+', help='List of classes', required=True)
+    args = parser.parse_args()
+
+    for cls in args.classes:
+        logging.info(f"Handled class: {cls}")
+        dir_path = os.path.join(args.data_dir, cls)
+        create_dir(dir_path)
+        download_images(dir_path, cls, 1, 1000)

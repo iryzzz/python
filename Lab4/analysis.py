6eb47caf8d5cf4ef3c14e79c62f278be4d9e3267
iryzzz <58042184+iryzzz@users.noreply.github.com>
2024-05-11 19:54:06 +0400

    fixed

--- Lab4/analysis.py
+++ Lab4/analysis.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+import logging
 import random
 
 import cv2
@@ -10,82 +12,117 @@ def get_dataframe(path: str) -> pd.DataFrame:
     """
     Получение датафрейма
     :param path: Путь к аннотации
-    :return:
+    :return: Считанный датафрейм
     """
-    return pd.read_csv(path, delimiter='\t', usecols=(0, 2), names=('AbsPath', 'Name'))
+    try:
+        return pd.read_csv(path, delimiter='\t', usecols=(0, 2), names=('AbsPath', 'Name'))
+    except Exception as exc:
+        logging.error(exc)
 
 
 def add_mark_col(df: pd.DataFrame) -> pd.DataFrame:
     """
     Добавление колонки с метками
+    :param df: Датафрейм без колонки с метками
+    :return: Датафрейм с добавленной колонкой с метками
     """
-    names = df['Name'].unique()
-    new_df = df.copy()
-    new_df["Mark"] = new_df["Name"].apply(lambda x: 0 if x == names[0] else 1)
-    return new_df
+    try:
+        names = df['Name'].unique()
+        new_df = df.copy()
+        new_df["Mark"] = new_df["Name"].apply(lambda x: 0 if x == names[0] else 1)
+        return new_df
+    except Exception as exc:
+        logging.error(exc)
 
 
 def add_size_col(df: pd.DataFrame) -> pd.DataFrame:
     """
     Добавление колонок с высотой, шириной и грубиной изображений
+    :param df: Датафрейм
+    :return: Датафрейм с добавленными колонками с высотой, шириной и грубиной изображений
     """
-    new_df = df.copy()
-    new_df["Height"] = new_df["AbsPath"].apply(lambda path: plt.imread(path).shape[0])
-    new_df["Width"] = new_df["AbsPath"].apply(lambda path: plt.imread(path).shape[1])
-    new_df["Channels"] = new_df["AbsPath"].apply(lambda path: plt.imread(path).shape[2])
-    return new_df
+    try:
+        new_df = df.copy()
+        new_df["Height"] = new_df["AbsPath"].apply(lambda path: plt.imread(path).shape[0])
+        new_df["Width"] = new_df["AbsPath"].apply(lambda path: plt.imread(path).shape[1])
+        new_df["Channels"] = new_df["AbsPath"].apply(lambda path: plt.imread(path).shape[2])
+        return new_df
+    except Exception as exc:
+        logging.error(exc)
 
 
-def statistic(df: pd.DataFrame):
+def statistic(df: pd.DataFrame) -> None:
     """
     Получение статистики
+    :param df: Датафрейм с колонками 'Height', 'Width', 'Channels' и 'Mark'
+    :return:
     """
-    for col in ['Height', 'Width', 'Channels']:
-        print(df[df['Mark'] == 0][col].describe(), '\n')
-        print(df[df['Mark'] == 1][col].describe(), '\n')
+    try:
+        for col in ['Height', 'Width', 'Channels']:
+            print(df[df['Mark'] == 0][col].describe(), '\n')
+            print(df[df['Mark'] == 1][col].describe(), '\n')
+    except Exception as exc:
+        logging.error(exc)
 
 
-def filter_by_class(df: pd.DataFrame, class_mark: int):
+def filter_by_class(df: pd.DataFrame, class_mark: int) -> pd.DataFrame:
     """
     Фильтр по классу
     :param df: Датафрейм
     :param class_mark: Метка класса
-    :return:
+    :return: Отфильтрованный датафрейм
     """
-    return df[df['Mark'] == class_mark]
+    try:
+        return df[df['Mark'] == class_mark]
+    except Exception as exc:
+        logging.error(exc)
 
 
-def filter_by_size(df: pd.DataFrame, class_mark: int, max_height: int, max_width: int):
+def filter_by_size(df: pd.DataFrame, class_mark: int, max_height: int,
+                   max_width: int) -> pd.DataFrame:
     """
     Фильтр по размеру изображения
     :param df: Датафрейм
     :param class_mark: Метка класса
     :param max_height: Максимальная высота изображения
     :param max_width: Максимальная ширина изображения
-    :return:
+    :return: Отфильтрованный датафрейм
     """
-    return df[(df['Mark'] == class_mark) & (df['Height'] <= max_height) & (df['Width'] <= max_width)]
+    try:
+        return df[(df['Mark'] == class_mark) & (df['Height'] <= max_height) & (df['Width'] <= max_width)]
+    except Exception as exc:
+        logging.error(exc)
 
 
 def add_pixels_col(df: pd.DataFrame) -> pd.DataFrame:
     """
     Добавление колонки с количеством пикселей
+    :param df: Датафрейм
+    :return: Датафрейм с добавленной колонкой с количеством пикселей
     """
-    new_df = df.copy()
-    new_df['Pixels'] = df['Height'] * df['Width'] * df['Channels']
-    return new_df
+    try:
+        new_df = df.copy()
+        new_df['Pixels'] = df['Height'] * df['Width'] * df['Channels']
+        return new_df
+    except Exception as exc:
+        logging.error(exc)
 
 
-def df_group(df: pd.DataFrame):
+def df_group(df: pd.DataFrame) -> None:
     """
     Статистика по количеству пикселей
+    :param df: Датафрейм
+    :return:
     """
-    for class_name, sub_df in df.groupby("Name"):
-        print(f'Name: {class_name}')
-        print(f'Max: {sub_df.Pixels.max()}')
-        print(f'Min: {sub_df.Pixels.min()}')
-        print(f'Mean: {sub_df.Pixels.mean()}')
-        print()
+    try:
+        for class_name, sub_df in df.groupby("Name"):
+            print(f'Name: {class_name}')
+            print(f'Max: {sub_df.Pixels.max()}')
+            print(f'Min: {sub_df.Pixels.min()}')
+            print(f'Mean: {sub_df.Pixels.mean()}')
+            print()
+    except Exception as exc:
+        logging.error(exc)
 
 
 def create_histogram(df: pd.DataFrame, class_mark: int) -> list[np.ndarray]:
@@ -95,37 +132,55 @@ def create_histogram(df: pd.DataFrame, class_mark: int) -> list[np.ndarray]:
     :param class_mark: Метка класса
     :return: Список гистограмм по трём каналам изображения
     """
-    new_df = filter_by_class(df, class_mark)
-    img = cv2.imread(new_df.iloc[random.randint(0, len(new_df) - 1)]['AbsPath'])
-    return [cv2.calcHist([img], [i], None, [256], [0, 256]) for i in range(3)]
+    try:
+        new_df = filter_by_class(df, class_mark)
+        img = cv2.imread(new_df.iloc[random.randint(0, len(new_df) - 1)]['AbsPath'])
+        return [cv2.calcHist([img], [i], None, [256], [0, 256]) for i in range(3)]
+    except Exception as exc:
+        logging.error(exc)
 
 
-def histogram(df: pd.DataFrame, class_mark: int):
+def histogram(df: pd.DataFrame, class_mark: int) -> None:
     """
     Отображение гистограммы
     :param df: Датафрейм
     :param class_mark: Метка класса
     :return:
     """
-    hist = create_histogram(df, class_mark)
-    plt.plot(hist[0], color='b')
-    plt.plot(hist[1], color='g')
-    plt.plot(hist[2], color='r')
-    plt.xlabel("Intensity")
-    plt.ylabel("Pixels")
-    plt.show()
+    try:
+        hist = create_histogram(df, class_mark)
+        plt.plot(hist[0], color='b')
+        plt.plot(hist[1], color='g')
+        plt.plot(hist[2], color='r')
+        plt.xlabel("Intensity")
+        plt.ylabel("Pixels")
+        plt.show()
+    except Exception as exc:
+        logging.error(exc)
 
 
 if __name__ == '__main__':
-    df = get_dataframe('annotation.csv')
-    print(df.tail(5))
-    df = add_mark_col(df)
-    print(df.head(5))
-    df = add_size_col(df)
-    print(df.head(5))
-    statistic(df)
-    print(filter_by_class(df, 1))
-    print(filter_by_size(df, 0, 300, 300))
-    df = add_pixels_col(df)
-    df_group(df)
-    histogram(df, 0)
+    logging.basicConfig(filename='lab4.log', level=logging.INFO)
+
+    parser = ArgumentParser()
+    parser.add_argument('annotation', type=str, help='Path to annotation')
+    parser.add_argument('-c', '--class_index', type=int, help='Class index of image', required=True)
+    parser.add_argument('-he', '--height', type=int, help='Height of image', required=True)
+    parser.add_argument('-w', '--width', type=int, help='Width of image', required=True)
+    args = parser.parse_args()
+
+    try:
+        df = get_dataframe(args.annotation)
+        print(df.tail())
+        df = add_mark_col(df)
+        print(df.head())
+        df = add_size_col(df)
+        print(df.head())
+        statistic(df)
+        print(filter_by_class(df, args.class_index))
+        print(filter_by_size(df, args.class_index, args.height, args.width))
+        df = add_pixels_col(df)
+        df_group(df)
+        histogram(df, args.class_index)
+    except Exception as exc:
+        logging.error(exc)

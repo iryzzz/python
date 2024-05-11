a7e2a551c9415406e62953c6eb18fd2e277244ee
iryzzz <58042184+iryzzz@users.noreply.github.com>
2024-05-11 19:53:25 +0400

    fix

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -1,18 +1,28 @@
+import json
 import os
 import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, \
+    QLabel, QHBoxLayout, QVBoxLayout, QMessageBox, QDesktopWidget, \
+    QAction, qApp, QFileDialog
 
 from annotation import create_annotation
 from copy_images import create_dataset_copy, create_copy_annotation
-from random_dataset import create_dataset_random, create_random_annotation
 from iterator import Iterator
+from random_dataset import create_dataset_random, create_random_annotation
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    """
+    Окно приложения
+    """
+    def __init__(self, settings: str):
+        """
+        Конструктор
+        :param settings: путь к json файлу с параметрами программы
+        """
         super().__init__()
 
         self.dataset_path = None
@@ -24,16 +34,40 @@ class MainWindow(QMainWindow):
 
         self.cat_iter = None
         self.dog_iter = None
+        self.classes = None
+        self.annotation_name = None
+        self.__set_settings(settings)
+
+    def __set_settings(self, settings: str):
+        """
+        Установка параметров программы
+        :param settings: путь к json файлу с параметрами программы
+        :return:
+        """
+        try:
+            with open(settings, "r") as read_file:
+                data = json.load(read_file)
+                self.annotation_name = data["annotation_path"]
+                self.classes = data["classes"]
+                if len(self.classes) != 2:
+                    raise ValueError("Неверное количество классов")
+        except:
+            self.get_message_box("Ошибка закрузки параметров программы")
+            sys.exit()
 
     def __init_ui(self):
+        """
+        Создание основных компонентов окна
+        :return:
+        """
         self.resize(1000, 800)
         self.center()
-        self.setWindowTitle('Cat and Dog')
+        self.setWindowTitle('Viewer')
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
 
-        cat_btn = QPushButton('Next cat', self)
-        dog_btn = QPushButton('Next dog', self)
+        cat_btn = QPushButton(f'Next {self.classes[0]}', self)
+        dog_btn = QPushButton(f'Next {self.classes[1]}', self)
         cat_btn.clicked.connect(self.next_cat)
         dog_btn.clicked.connect(self.next_dog)
 
@@ -54,17 +88,42 @@ class MainWindow(QMainWindow):
 
     @staticmethod
     def get_message_box(message: str):
+        """
+        Создание QMessageBox
+        :param message: Сообщение
+        :return:
+        """
         mb = QMessageBox()
         mb.setWindowTitle("Message")
         mb.setText(message)
         mb.exec()
 
-    def next_cat(self):
+    def check_dataset(self) -> bool:
+        """
+        Проверка наличия выбранного датасета
+        :return: True - датасет выбран, False - иначе
+        """
         if self.dataset_path is None:
             self.get_message_box("Please, select dataset")
-            return
+            return False
+        return True
+
+    def check_annotation(self) -> bool:
+        """
+        Проверка наличия выбранной аннотации
+        :return: True - аннотация выбрана, False - иначе
+        """
         if self.annotation_path is None:
             self.get_message_box("Please, create annotation")
+            return False
+        return True
+
+    def next_cat(self):
+        """
+        Получение следующего изображения кота
+        :return:
+        """
+        if not self.check_dataset() and not self.check_annotation():
             return
         lbl_size = self.lbl.size()
 
@@ -75,15 +134,15 @@ class MainWindow(QMainWindow):
             self.lbl.setPixmap(img)
             self.lbl.setAlignment(Qt.AlignCenter)
         except:
-            self.cat_iter = Iterator("cat", self.annotation_path)
+            self.cat_iter = Iterator(self.classes[0], self.annotation_path)
             self.next_cat()
 
     def next_dog(self):
-        if self.dataset_path is None:
-            self.get_message_box("Please, select dataset")
-            return
-        if self.annotation_path is None:
-            self.get_message_box("Please, create annotation")
+        """
+        Получение следующего изображения собаки
+        :return:
+        """
+        if not self.check_dataset() and not self.check_annotation():
             return
 
         lbl_size = self.lbl.size()
@@ -94,63 +153,81 @@ class MainWindow(QMainWindow):
             self.lbl.setPixmap(img)
             self.lbl.setAlignment(Qt.AlignCenter)
         except:
-            self.dog_iter = Iterator("dog", self.annotation_path)
+            self.dog_iter = Iterator(self.classes[1], self.annotation_path)
             self.next_dog()
 
     def center(self):
+        """
+        Выравнивание по центру
+        :return:
+        """
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
     def __create_menu_bar(self):
+        """
+        Создание панели меню
+        :return:
+        """
         menu_bar = self.menuBar()
-        self.fileMenu = menu_bar.addMenu('&File')
+        self.fileMenu = menu_bar.addMenu('File')
         self.fileMenu.addAction(self.exitAction)
         self.fileMenu.addAction(self.changeAction)
 
-        self.annotMenu = menu_bar.addMenu('&Annotation')
+        self.annotMenu = menu_bar.addMenu('Annotation')
         self.annotMenu.addAction(self.createAnnotAction)
 
-        self.dataMenu = menu_bar.addMenu('&Datasets')
+        self.dataMenu = menu_bar.addMenu('Datasets')
         self.dataMenu.addAction(self.createData2Action)
 
     def __create_action(self):
-        self.exitAction = QAction('&Exit')
+        """
+        Создание и подключение событий окна
+        :return:
+        """
+        self.exitAction = QAction('Exit')
         self.exitAction.triggered.connect(qApp.quit)
 
-        self.changeAction = QAction('&Select dataset')
+        self.changeAction = QAction('Select dataset')
         self.changeAction.triggered.connect(self.select_dataset)
 
-        self.createAnnotAction = QAction('&Create annotation for current dataset')
+        self.createAnnotAction = QAction('Create annotation for current dataset')
         self.createAnnotAction.triggered.connect(self.__create_annotation)
 
-        self.createData2Action = QAction('&Create copy dataset')
+        self.createData2Action = QAction('Create copy dataset')
         self.createData2Action.triggered.connect(self.create_copy_dataset)
 
-        self.createData3Action = QAction('&Create random dataset')
+        self.createData3Action = QAction('Create random dataset')
         self.createData3Action.triggered.connect(self.create_random_dataset)
 
     def __create_annotation(self):
-        if self.dataset_path is None:
-            self.get_message_box("Please, select dataset")
+        """
+        Создание аннотации к датасету
+        :return:
+        """
+        if not self.check_dataset():
             return
         self.annotation_path = QFileDialog.getSaveFileName(self, 'Create file', filter="(*.csv)")[0]
-        create_annotation(self.dataset_path, ["cat", "dog"], self.annotation_path)
+        create_annotation(self.dataset_path, self.classes, self.annotation_path)
         mb = QMessageBox()
         mb.setWindowTitle("Message")
         mb.setText("Task completed")
         mb.exec()
 
     def create_copy_dataset(self):
-        if self.dataset_path is None:
-            self.get_message_box("Please, select base dataset")
+        """
+        Создание копии датасета и его аннотации
+        :return:
+        """
+        if not self.check_dataset():
             return
         self.dataset_copy_path = QFileDialog.getExistingDirectory(self, 'Select Folder')
         self.dataset_copy_path = os.path.relpath(self.dataset_copy_path)
-        create_dataset_copy(self.dataset_path, self.dataset_copy_path, ["cat", "dog"])
-        create_copy_annotation(self.dataset_copy_path, ["cat", "dog"],
-                               os.path.join(self.dataset_copy_path, "annotation.csv"))
+        create_dataset_copy(self.dataset_path, self.dataset_copy_path, self.classes)
+        create_copy_annotation(self.dataset_copy_path, self.classes,
+                               os.path.join(self.dataset_copy_path, self.annotation_name))
 
         self.dataMenu.addAction(self.createData3Action)
         mb = QMessageBox()
@@ -159,25 +236,37 @@ class MainWindow(QMainWindow):
         mb.exec()
 
     def create_random_dataset(self):
-        if self.dataset_path is None:
-            self.get_message_box("Please, select base dataset")
+        """
+        Создание датасета с перемешанными изображениями
+        :return:
+        """
+        if not self.check_dataset():
             return
 
         dataset_path = QFileDialog.getExistingDirectory(self, 'Select Folder')
         dataset_path = os.path.relpath(dataset_path)
-        classes_dict = create_dataset_random(self.dataset_copy_path, dataset_path, ["cat", "dog"])
+        classes_dict = create_dataset_random(self.dataset_copy_path, dataset_path, self.classes)
         create_random_annotation(dataset_path, classes_dict,
-                                 os.path.join(dataset_path, "annotation.csv"))
+                                 os.path.join(dataset_path, self.annotation_name))
         mb = QMessageBox()
         mb.setWindowTitle("Message")
         mb.setText("Task completed")
         mb.exec()
 
     def select_dataset(self):
+        """
+        Выбор датасета
+        :return:
+        """
         self.dataset_path = QFileDialog.getExistingDirectory(self, 'Select Folder')
         self.dataset_path = os.path.relpath(self.dataset_path)
 
     def closeEvent(self, event):
+        """
+        Событие закрытия окна
+        :param event:
+        :return:
+        """
         reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes |
                                      QMessageBox.No)
         if reply == QMessageBox.Yes:
@@ -188,5 +277,5 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    win = MainWindow()
+    win = MainWindow("settings.json")
     sys.exit(app.exec_())
